7d9fd6cc28757934314edd7f618282794f8eceb1
spettigrew <sara.pettigrew5@gmail.com>
2021-02-12 18:57:11 -0700

    Added axios and form submit

--- user-board/src/App.js
+++ user-board/src/App.js
@@ -1,6 +1,8 @@
-import { useState } from 'react';
-import * yup from 'yup';
+import { useState, useEffect } from 'react';
+import * as yup from 'yup';
 import './App.css';
+import Form from './Form';
+import axios from 'axios';
 
 function App() {
   const validationSchema = yup.object().shape({
@@ -9,16 +11,10 @@ function App() {
       .email('Please include valid email')
       .required('Email is required'),
     password: yup.string().required('Password is required'),
+    // expecting true as one of our parameters, otherwise return false
     terms: yup.boolean().oneOf([true], 'Agree to term before proceeding')
   })
 
-  useEffect(() => {
-    validationSchema.isValid(form)
-    .then((valid) => {
-      setValidated(valid)
-    })
-  }, [form])
-  
   const [validated, setValidated] = useState(false);
   const [form, setForm] = useState({
     name: '',
@@ -34,11 +30,12 @@ function App() {
     terms: '',
   })
 
+  
   const validation = (e) => {
     // set value to the target.value if not checkbox and target.checked if checkbox
     let value = 
       e.target.type === 'checkbox' ? e.target.checked : e.target.value;
-      // yup is ansync
+      // yup is async
       yup
       // yup.reach passing in the validationSchema to check against the event.target
         .reach(validationSchema, e.target.name)
@@ -61,7 +58,35 @@ function App() {
             [e.target.name]: err.errors[0]
           })
         })
-  }
+      }
+      
+      const handlePost = (form) => {
+        axios.post('https://regres.in/api/users', form)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+      }
+
+      useEffect(() => {
+        validationSchema.isValid(form)
+          .then((valid) => {
+            setValidated(valid)
+          })
+      }, [form])
+
+      const formSubmit = (e) => {
+        e.preventDefault();
+        handlePost(form); 
+        setForm( {
+          name: '',
+          email: '',
+          password: '',
+          terms: false,
+        })
+      }
 
   const handleChanges = (e) => {
     setForm({
@@ -70,36 +95,45 @@ function App() {
     })
   };
 
-  const formSubmit = () => {
-
-  };
-
-  console.log({ form });
 
   return (
     <div className="App">
       <form onSubmit={formSubmit}>
+        <label htmlFor={'name'}>
         <input
+          id={'name'}
           type={'text'}
           name={'name'}
           value={form.name}
           placeholder={'Name'}
           onChange={handleChanges}
         />
+        {/* if there is an error, it will display within the label */}
+        {errors.name.length > 0 && <p>{errors.name}</p> }
+        </label>
+        {/* htmlFor points to an id for one specific item */}
+        <label htmlFor={'password'}>
         <input 
+          id={'password'}
           type={'password'}
           name={'password'}
           value={form.password}
           placeholder={"Password"}
           onChange={handleChanges}
           />
+          {errors.name.length > 0 && <p>{errors.password}</p> }
+          </label>
+          <label htmlFor={'email'}>
         <input
+          id={'email'}
           type={'text'}
           name={'email'}
           value={form.email}
           placeholder={'Email'}
           onChange={handleChanges}
         />
+        {errors.name.length > 0 && <p>{errors.email}</p> }
+        </label>
         <label
           for={'terms'}>Agree to terms</label>
         <input
